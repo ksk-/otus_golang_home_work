fb6ba04cc46ed3fa3fa521b0d59df9ce95976f81
ksk- <iksk810@yandex.ru>
2022-10-25 13:01:55 +0300

    HW8 is completed

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		log.Fatalf("usage error: not enough args to run app")
+	}
+
+	dir := os.Args[1]
+	env, err := ReadDir(dir)
+	if err != nil {
+		log.Fatalf("failed to read env dir: %v", err)
+	}
+
+	cmd := os.Args[2]
+	args := os.Args[3:]
+	os.Exit(RunCmd(append([]string{cmd}, args...), env))
 }
